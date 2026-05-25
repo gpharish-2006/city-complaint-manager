53826b97a12ecbeac0ff749b909ac54213739770
lovelyharish06 <gpharish2006@gmail.com>
2026-05-25 16:20:05 +0530

    Final

--- server/server.mjs
+++ server/server.mjs
@@ -11,12 +11,16 @@ const port = process.env.PORT || 5000;
 
 const app = express()
 
-app.use(cors({
-  origin: [
+app.use(
+  cors({
+    origin: [
     "http://localhost:5173", 
     "https://resolve-net.vercel.app"
   ],
-}));
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use("/api/complaints", complaintRoutes)
