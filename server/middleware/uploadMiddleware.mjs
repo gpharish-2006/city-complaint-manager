55fe9f311ae2cb1c9d7a81a6f095f4793b47aa7e
lovelyharish06 <gpharish2006@gmail.com>
2026-05-25 12:15:22 +0530

    Final

--- server/middleware/uploadMiddleware.mjs
+++ server/middleware/uploadMiddleware.mjs
@@ -1,25 +1,31 @@
-import multer from 'multer';
-import path from 'path';
+import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
-    fileFilter: (req,file,cb)=>{
-        const allowedType = ['image/jpeg','image/png','image/jpg'];
-        if(allowedType.includes(file.mimetype)){
-            cb(null,true);
-        }
-        else{
-            cb(new Error("Invalid Image type"));
-        }
-    },
-    destination: (req,file,cb)=>{
-        cb(null,path.join(process.cwd(),'uploads'))
-    },
-    filename:(req,file,cb)=>{
-        const uniqueName = Date.now() + "-" + file.originalname;
-        cb(null,uniqueName);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null,path.join(process.cwd(), "uploads"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
 
-const upload = multer({storage:storage});
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const allowedType = [
+      "image/jpeg",
+      "image/png",
+      "image/jpg",
+    ];
+    if (allowedType.includes(file.mimetype)) {
+      cb(null, true);
+    }
+    else {
+      cb(new Error("Invalid Image Type"),false);
+    }
+  },
+});
 
 export default upload;
